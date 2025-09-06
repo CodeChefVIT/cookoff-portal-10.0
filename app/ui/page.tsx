e313619e32e91e51a43d6277531c5f0b908b804c
Upayan M <upayanm3@gmail.com>
2025-09-06 19:55:12 +0530

    feat: implement API route and UI page with styled buttons

---
+++ app/ui/page.tsx
@@ -0,0 +1,8 @@
+export default function UIPage() {
+  return (
+    <div className="p-4">
+      <button className="button">Click Me</button>
+      <button className="btn-green">Green Button</button>
+    </div>
+  );
+}
