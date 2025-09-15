9e1ed6e799685d45f0b3237797f43a8c8df6905f
Nyasa <nyasa@gmail.com>
2025-09-15 12:00:09 +0530

    Dashboard page added with grids for other components except  statistic and header

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
+import Dashboard from "@/components/Dashboard/Dashboard";
 
 export default function Home() {
   return (
-    <div className="min-h-screen">
-      Check out the UI page <Link href="/ui">here</Link>.
-    </div>
+    <Dashboard/>
+    // <div className="min-h-screen">
+    //   Check out the UI page <Link href="/ui">here</Link>.
+    // </div>
   );
 }
 
