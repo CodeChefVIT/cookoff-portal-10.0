64b01d2d503633d3e46bd5e088dea0e6fa5da330
Nyasa <nyasa@gmail.com>
2025-09-15 12:03:31 +0530

    Removed the changes made in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,9 @@ import Dashboard from "@/components/Dashboard/Dashboard";
 
 export default function Home() {
   return (
-    <Dashboard/>
-    // <div className="min-h-screen">
-    //   Check out the UI page <Link href="/ui">here</Link>.
-    // </div>
+    <div className="min-h-screen">
+      Check out the UI page <Link href="/ui">here</Link>.
+    </div>
   );
 }
 
