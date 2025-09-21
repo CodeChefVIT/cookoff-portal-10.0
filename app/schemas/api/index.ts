d05f2f65589e69ad339c66318802225e2a50d91e
unknown <sanjana.shyamsundar55@gmail.com>
2025-09-22 02:09:04 +0530

    Fix : Response of questions API

--- app/schemas/api/index.ts
+++ app/schemas/api/index.ts
@@ -1,21 +1,22 @@
-import { Question } from "../api";
-
 export interface ApiResponse {
   message: string;
   data: unknown;
 }
 
-
-export interface QuestionTestcasePair {
-  question: Question;
-  testcases: Testcase[];
-}
-
-interface QuestionWindowProps {
-  questions: Question[];
-  setQuestions: React.Dispatch<React.SetStateAction<Question[]>>;
-  setQuestionID: React.Dispatch<React.SetStateAction<string>>; // <- string not number
-  questionID: string;
+export interface Question {
+  id: string; 
+  title: string;
+  points: number; 
+  description: string;
+  inputFormat: string[];
+  constraints: string[];
+  outputFormat: string[];
+  sampleTestInput: string[];
+  sampleTestOutput: string[];
+  explanation: string[];
+  qType: string;
+  round: number; 
+  isBountyActive: boolean;
 }
 
 
@@ -27,4 +28,19 @@ export interface Testcase {
   hidden: boolean;
   runtime: number; // double
   question_id: string; // uuid
+  output?: string;
+}
+
+
+export interface QuestionTestcasePair {
+  question: Question;
+  testcases: Testcase[];
+}
+
+
+export interface QuestionWindowProps {
+  questions: Question[];
+  setQuestions: React.Dispatch<React.SetStateAction<Question[]>>;
+  setQuestionID: React.Dispatch<React.SetStateAction<string>>; // string, matches API
+  questionID: string;
 }
