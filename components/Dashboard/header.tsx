d4bb16a86c014be6972675483dcce759df68b5df
unknown <sanjana.shyamsundar55@gmail.com>
2025-09-23 13:16:11 +0530

    Feat: CC logo redirects to website

--- components/Dashboard/header.tsx
+++ components/Dashboard/header.tsx
@@ -2,8 +2,13 @@
 import Image from "next/image";
 export default function Header() {
     return(<header className="flex items-center justify-between h-[150px] w-full px-8">
+        <a
+        href="https://www.codechefvit.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <Image src="/ccLogo.svg" alt="Logo" width={75} height={75} className="opacity-100" priority />
-
+</a>
         <h1 className="text-[90px] font-bold text-center font-[Nulshock] flex items-center justify-center gap-4">
         <span className="text-[#B7AB98] px-2 py-1">COOK OFF</span>
         <span className="bg-[linear-gradient(90deg,#32CA67_83.17%,#0A8A36_92.79%)] bg-clip-text text-transparent px-2 py-1">10.0</span>
