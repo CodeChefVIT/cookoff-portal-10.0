e15daa82e05d218f629973169e3d290af0a54c9f
Nyasa <nyasa@gmail.com>
2025-09-25 18:09:39 +0530

    Header changed

--- app/services/dashboard.ts
+++ app/services/dashboard.ts
@@ -3,7 +3,6 @@ import { RoundStats } from "@/components/Dashboard/Statistics/statistics";
 import { DashboardResponse } from "../schemas/api/index";
 
 export async function getRounds(): Promise<RoundStats[]> {
-  console.log("api:", api);
 
   const res = await api.get("/dashboard");
   const data = res.data.data;
@@ -15,8 +14,6 @@ export async function getRounds(): Promise<RoundStats[]> {
     const score = (data as any)[`round${i}Score`] ?? 0;
 
     let status: "Closed" | "In Progress" | "Completed" = "Closed";
-    console.log("here",data.current_round);
-    console.log(i);
     if (i < data.current_round) status = "Completed";
     else if (i === data.current_round) status = "In Progress";
 
