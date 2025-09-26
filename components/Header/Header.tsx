f8d95aa976383908eb10688b56f323ad43604e93
Nyasa <nyasa@gmail.com>
2025-09-26 19:38:11 +0530

    dashboard button on header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,14 +3,32 @@
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
 import NotificationButton from "../ui/NotificationButton";
+import toast from "react-hot-toast";
 
 export default function Header() {
-  const icon = "/icon.avif";
-  const pathname = usePathname();
-  if (pathname === "/") return null;
 
+  const icon = "/icon.avif";
+  const dashboardIcon = "/dashboard.svg";
+  const pathname = usePathname();
+    const router = useRouter();
+
+  if (pathname === "/") return null;
+  const handleDashboardClick = () => {
+    if (pathname === "/dashboard") return;
+
+    const toastId = toast.loading("Entering Dashboard...");
+
+    router.push("/dashboard");
+
+    const checkPath = setInterval(() => {
+      if (window.location.pathname === "/dashboard") {
+        toast.success("Welcome to Dashboard", { id: toastId });
+        clearInterval(checkPath);
+      }
+    }, 100);
+  };
   return (
-    <header className="flex items-center justify-between w-full px-4 py-2 md:px-12 border-b-2 border-b-[#B7AB98] select-none">
+    <header className="relative flex items-center justify-between w-full px-4 py-1/2 md:px-12 border-b-2 border-b-[#B7AB98] select-none">
       <a
         href="https://www.codechefvit.com"
         target="_blank"
@@ -25,7 +43,7 @@ export default function Header() {
         />
       </a>
 
-      <h1 className="font-nulshock font-bold text-2xl md:text-4xl lg:text-5xl capitalize text-center text-[#B7AB98]">
+      <h1 className="absolute left-1/2 -translate-x-1/2 font-nulshock font-bold text-2xl md:text-4xl lg:text-5xl capitalize text-center text-[#B7AB98]">
         COOK {""}
         <span
           className="inline-block bg-clip-text text-transparent"
@@ -38,8 +56,22 @@ export default function Header() {
         </span>
       </h1>
 
-      <div className="relative">
+      <div className="flex items-center gap-4 ml-auto">
         <NotificationButton />
+        {pathname === "/kitchen" && (
+          <button
+            onClick={handleDashboardClick}
+            className="focus:outline-none !bg-transparent"
+          >
+            <Image
+              src={dashboardIcon}
+              alt="Dashboard"
+              width={20}
+              height={20}
+              className="h-4 w-4 md:h-12 md:w-12"
+            />
+          </button>
+        )}
       </div>
     </header>
   );
