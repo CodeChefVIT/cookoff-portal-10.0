3c1c19b69c048b93a20836a98b78d41bd8f60ac9
abhitrueprogrammer <abhiaruvs3@gmail.com>
2025-09-27 22:15:35 +0530

    your output, input only show

--- components/TestCases/Input.tsx
+++ components/TestCases/Input.tsx
@@ -45,11 +45,11 @@ const Input: React.FC<InputProps> = ({
       {activeCaseData && (
         <>
           {activeCaseData.output ? (
-            <div className="flex justify-between font-inter">
+            <div className="flex justify-between font-inter gap-4">
               <InputOutputCard
                 title={"Input"}
                 data={activeCaseData.input}
-                className={"w-[31%]"}
+                className="w-full"
               />
               <InputOutputCard
                 title={"Your Output"}
@@ -58,7 +58,7 @@ const Input: React.FC<InputProps> = ({
                     ? activeCaseData.output
                     : "no output given"
                 }
-                className={"w-[31%]"}
+                className="w-full"
               />
             </div>
           ) : (
